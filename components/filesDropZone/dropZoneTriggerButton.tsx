bbbadb0577de649fe75f75c6afa1e9e1c574f6a2
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-03-29 11:33:41 +0100

    feat: added accernity button

--- components/filesDropZone/dropZoneTriggerButton.tsx
+++ components/filesDropZone/dropZoneTriggerButton.tsx
@@ -1,23 +1,28 @@
 "use client";
 
 import { Upload } from "lucide-react";
-import { Button } from "@/components/ui/button";
 import FileDropZone from "@/components/filesDropZone/fileDropZone";
 import { useState } from "react";
+import { HoverBorderGradient } from "../ui/hover-border-gradient";
 
 export default function DropZoneTriggerButton() {
   const [isDropZoneOpen, setIsDropZoneOpen] = useState(false);
 
   return (
     <div>
-      <Button
+      <div
+        className=" flex justify-center text-center"
         onClick={() => setIsDropZoneOpen((prev) => !prev)}
-        className="bg-brand hover:bg-brand/90 text-white"
       >
-        <Upload className="mr-2 h-4 w-4" />
-        <span>Upload</span>
-      </Button>
-
+        <HoverBorderGradient
+          containerClassName="rounded-full"
+          as="button"
+          className=" bg-black  text-white flex items-center space-x-2"
+        >
+          <Upload className="mr-2 h-4 w-4" />
+          <span>Upload</span>
+        </HoverBorderGradient>
+      </div>
       <FileDropZone
         isDropZoneOpen={isDropZoneOpen}
         setIsDropZoneOpen={setIsDropZoneOpen}
