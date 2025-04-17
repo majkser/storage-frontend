9896138079fe9fa03cf2e178a65a7e637760cbb0
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-04-17 10:46:38 +0200

    fix: path

--- components/filesDropZone/fileDropZone.tsx
+++ components/filesDropZone/fileDropZone.tsx
@@ -49,18 +49,22 @@ export default function FileDropZone({
     });
 
     try {
-      await axios.post("api/files/upload", formData, {
-        withCredentials: true,
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-        onUploadProgress: (ProgressEvent) => {
-          const percentCompleted = Math.round(
-            (ProgressEvent.loaded * 100) / (ProgressEvent.total || 1)
-          );
-          setUploadProgress(percentCompleted);
-        },
-      });
+      await axios.post(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/files/upload`,
+        formData,
+        {
+          withCredentials: true,
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+          onUploadProgress: (ProgressEvent) => {
+            const percentCompleted = Math.round(
+              (ProgressEvent.loaded * 100) / (ProgressEvent.total || 1)
+            );
+            setUploadProgress(percentCompleted);
+          },
+        }
+      );
 
       setFiles([]);
       setUploadProgress(0);
