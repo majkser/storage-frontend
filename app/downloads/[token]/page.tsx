fc2262175d1854beea13110ace3d85c6d3b7aa32
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-05-09 09:55:57 +0200

    fix: improved ui

--- app/downloads/[token]/page.tsx
+++ app/downloads/[token]/page.tsx
@@ -5,6 +5,7 @@ import { AiOutlineFileUnknown } from "react-icons/ai";
 import { FaPhotoVideo } from "react-icons/fa";
 import { LuFileMusic } from "react-icons/lu";
 import DownloadButton from "@/components/downloads/downloadButton";
+import { Button } from "@/components/ui/button";
 
 export default async function page({
   params,
@@ -17,16 +18,31 @@ export default async function page({
     <BackgroundBeamsWithCollision className="h-screen w-screen">
       <div className="z-20 flex h-screen w-screen flex-col items-center justify-center gap-4">
         <h1 className="h1 text-white text-center">{token}</h1>
-        <ScrollArea className="h-[500px] w-[500px] bg-black/75">
+        <ScrollArea className="h-3/4 w-3/5 bg-black/75">
           <div className="flex flex-col items-center justify-center gap-4 p-4 text-white">
             <h1 className="h1">Token</h1>
-            <Separator />
-            <p className="">
-              This is your token. Please copy it and save it somewhere safe.
-            </p>
-            <Separator />
-            <p className="">{token}</p>
-            <Separator />
+            <ul className="w-full">
+              <li>
+                <div className="flex w-full justify-between items-center mb-1">
+                  <div className="flex items-center gap-4">
+                    <FaPhotoVideo size={25} className="text-brand" />
+                    <span className="span text-gray-400">File name</span>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <p className="text-sm text-gray-400">1.77 GB</p>
+                    <Button variant="link" className="text-brand">
+                      Download
+                    </Button>
+                  </div>
+                </div>
+                <Separator />
+              </li>
+              <li>
+                <p className="text-sm text-gray-400">File name</p>
+                <p className="text-sm text-gray-400">File size</p>
+                <Separator />
+              </li>
+            </ul>
             <AiOutlineFileUnknown size={25} className="text-brand" />
             <FaPhotoVideo size={25} className="text-brand" />
             <LuFileMusic size={25} className="text-brand" />
