ab63755a16f22fcb21563e987d58e3bff8af0767
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-05-11 11:53:16 +0200

    fix & feat: moved link generating to the frontend and display it and give user ability to copy it

--- components/generateLinkButton.tsx
+++ components/generateLinkButton.tsx
@@ -1,23 +1,42 @@
 import axios from "axios";
 import { Popover, PopoverTrigger, PopoverContent } from "./ui/popover";
-import { Button } from "./ui/button";
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
+import { Copy } from "lucide-react";
 
-async function generateLink(fileId: number) {
-  try {
-    axios.post(
-      `${process.env.NEXT_PUBLIC_SERVER_URL}/api/link/generate-link`,
-      { fileId: fileId },
-      {
-        withCredentials: true,
-      }
-    );
-  } catch (error) {
-    console.error("Error generating link:", error);
-  }
-}
+//TODO popover - must be wider !!!!
 
 export default function GenerateLinkButton() {
+  const [link, setLink] = useState("");
+  const [isCopied, setIsCopied] = useState(false);
+
+  function handleCopyButtonClick() {
+    navigator.clipboard.writeText(
+      `${process.env.NEXT_PUBLIC_DOWNLOAD_URL}/${link}`
+    );
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 2000);
+  }
+
+  async function generateLink(fileId: number) {
+    const token = uuidv4();
+    setLink(token);
+
+    try {
+      axios.post(
+        `${process.env.NEXT_PUBLIC_SERVER_URL}/api/link/generate-link`,
+        { fileId: fileId, token: token },
+        {
+          withCredentials: true,
+        }
+      );
+    } catch (error) {
+      console.error("Error generating link:", error);
+    }
+  }
+
   return (
     <div>
       <Popover>
@@ -25,8 +44,18 @@ export default function GenerateLinkButton() {
           Generate Link
         </PopoverTrigger>
         <PopoverContent>
-          <div>
-            <p>Link: </p>
+          <p>Link: {`${process.env.NEXT_PUBLIC_DOWNLOAD_URL}/${link}`}</p>
+          <div className="flex justify-start">
+            <Copy
+              size="25"
+              className="mt-2 p-1 scale-125 rounded-sm hover:bg-gray-200 cursor-pointer"
+              onClick={handleCopyButtonClick}
+            />
+            {isCopied && (
+              <p className="z-20 bg-gray-200 rounded-sm p-1.5 m-auto caption">
+                copied to clipboard
+              </p>
+            )}
           </div>
         </PopoverContent>
       </Popover>
