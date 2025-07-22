a521729fa1ba9c3d54e1def564710821fbf3a461
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-07-23 00:21:14 +0200

    feat: added admin panel and wraped dashboard and admin with layout

---
+++ app/(main)/dashboard/page.tsx
@@ -0,0 +1,22 @@
+import { SearchBar } from "@/components/dashboard/search-bar";
+import { UploadButton } from "@/components/dashboard/upload-button";
+import { MobileMenu } from "@/components/sidebar/mobile-menu";
+import UploadedFiles from "@/components/dashboard/uploadedFiles";
+
+export default function DashboardPage() {
+  return (
+    <>
+      <div className="p-4 md:hidden">
+        <MobileMenu />
+      </div>
+      <div className="p-6 pt-0 md:pt-6 max-w-[1200px] mx-auto">
+        {/* Header with search and upload */}
+        <div className="flex justify-between items-center mb-8 max-w-[1400px]">
+          <SearchBar />
+          <UploadButton />
+        </div>
+      </div>
+      <UploadedFiles />
+    </>
+  );
+}
