b8fbfe5fbeedf0358ecc08a74418a0de48156571
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-07-24 20:28:26 +0200

    fix: improved responsivness

--- app/(main)/dashboard/page.tsx
+++ app/(main)/dashboard/page.tsx
@@ -11,7 +11,7 @@ export default function DashboardPage() {
       </div>
       <div className="p-6 pt-0 md:pt-6 max-w-[1200px] mx-auto">
         {/* Header with search and upload */}
-        <div className="flex justify-between items-center mb-8 max-w-[1400px]">
+        <div className="flex justify-between items-center mb-8 max-w-[1400px] min-w-[300px]">
           <SearchBar />
           <UploadButton />
         </div>
--- components/dashboard/uploadedFiles.tsx
+++ components/dashboard/uploadedFiles.tsx
@@ -23,8 +23,8 @@ export default function UploadedFiles() {
   };
 
   return (
-    <div className="w-full">
-      <div className="ml-4 md:ml-24 mt-4 mb-8">
+    <div className="w-full min-w-[350px]">
+      <div className="ml-4 md:ml-24 mb-8">
         <h1 className="text-white h2">Uploaded Files</h1>
         <SortAndFilter
           handleSortChange={handleSortChange}
