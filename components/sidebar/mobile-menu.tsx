35b8cae642cacc9e754f925c7ebef362d70ae48b
Mikołaj Kowalski <mikser.kowalski@gmail.com>
2025-07-24 20:53:38 +0200

    fix: hamburger icon made visible

--- components/sidebar/mobile-menu.tsx
+++ components/sidebar/mobile-menu.tsx
@@ -1,21 +1,21 @@
-"use client"
+"use client";
 
-import { Menu } from 'lucide-react'
-import { Button } from "@/components/ui/button"
-import { useSidebarContext } from "@/components/sidebar/sidebar-context"
+import { Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useSidebarContext } from "@/components/sidebar/sidebar-context";
 
 export function MobileMenu() {
-  const { toggleSidebar } = useSidebarContext()
+  const { toggleSidebar } = useSidebarContext();
 
   return (
-    <Button 
-      variant="ghost" 
-      size="icon" 
-      className="md:hidden" 
+    <Button
+      variant="ghost"
+      size="lg"
+      className="md:hidden aspect-square group"
       onClick={toggleSidebar}
     >
-      <Menu className="h-6 w-6" />
+      <Menu className="scale-150 text-primary-foreground group-hover:text-black" />
       <span className="sr-only">Open menu</span>
     </Button>
-  )
+  );
 }
